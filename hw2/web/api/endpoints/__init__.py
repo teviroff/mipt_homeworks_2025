4b6d7f17e4ab99e387aef32f4d7d6da0365ff82d
teviroff <mkaluzhnyy@mail.ru>
2025-12-17 19:34:36 +0300

    solution for `hw2`

---
+++ hw2/web/api/endpoints/__init__.py
@@ -0,0 +1,5 @@
+"""Main APIs of the project."""
+
+from hw2.web.api.endpoints.views import router
+
+__all__ = ["router"]
---
+++ infrastructure/__init__.py
@@ -0,0 +1,6 @@
+"""Docstring for infrastructure."""
+
+from infrastructure.client import GitHubClient
+from infrastructure.models import Repository
+
+__all__ = ["GitHubClient", "Repository"]
